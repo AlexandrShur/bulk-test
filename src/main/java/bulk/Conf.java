e253f5bf62848713a560e9f86d714166bf0569b3
Alexandr Shchurenkov <alexandr.shchurenkov@akvelon.com>
2017-04-20 20:55:54 +0300

    Add gzip test.

--- src/main/java/bulk/Conf.java
+++ src/main/java/bulk/Conf.java
@@ -1,13 +1,9 @@
 package bulk;
 
 import com.github.ziplet.filter.compression.CompressingFilter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import javax.servlet.Filter;
-import java.util.Arrays;
 
 @Configuration
 public class Conf {
--- src/test/bulk/BulkServiceApplicationTests.java
+++ src/test/bulk/BulkServiceApplicationTests.java
@@ -1,34 +1,55 @@
 package bulk;
 
+import bulk.dto.BulkResponse;
 import bulk.dto.Role;
 import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.google.common.io.ByteStreams;
+import com.google.common.io.ByteStreams;
+import org.apache.tomcat.util.ExceptionUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.*;
+import javax.servlet.descriptor.JspConfigDescriptor;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.hasSize;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.*;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+import java.util.zip.ZipEntry;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BulkServiceApplicationTests {
-
 
     @Autowired
     Database database;
@@ -36,51 +57,25 @@ public class BulkServiceApplicationTests {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
+
     private MockMvc mvc;
 
     public static final String CONTRACT_BASE_URI_WILCO = AppRolesResource.CONTRACT_BASE_URI.replace("{tenant}", "Wilco") + "/_bulk";
 
-
     @Before
     public void setUp() throws Exception {
-        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .build();
         database.resetDatabase();
     }
 
     @After
     public void tearDown() throws Exception {
-    }
-
-    @Test
-    public void gzipCompression_CompressedRequest_Compresssedresponse() throws Exception {
-
-        // Arrange
-        String urlTemplate =  CONTRACT_BASE_URI_WILCO;
-        List<Role> roles = new ArrayList<>();
-        int failedRoleCount = 1;
-        int acceptedRoleCount = 1;
-        roles.add(new Role(0, "SomeName", "Some desciption", "Some"));
-        roles.add(new Role(0, "SomeName", "Some desciption", "Some"));
-        MockHttpServletRequestBuilder msb = post(urlTemplate)
-                .content(toJsonString(roles))
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("Accept-Encoding", "gzip")
-                .accept(MediaType.APPLICATION_JSON);
-
-        // Act
-        ResultActions resultActions = mvc.perform(msb);
-
-        for (String name : resultActions.andReturn().getResponse().getHeaderNames()) {
-            System.out.println("reponsegzip: " + name);
-        }
-        // Assert
-        resultActions.andExpect(status().isOk())
-                .andExpect(jsonPath("$.rejectedRoles", hasSize(failedRoleCount)))
-                .andExpect(jsonPath("$.acceptedRoles", hasSize(acceptedRoleCount)))
-                .andExpect(jsonPath("$.acceptedRoles.[0].id", is(1)))
-                .andExpect(jsonPath("$.rejectedRoles.[0].role.id", is(0)))
-                .andExpect(jsonPath("$.rejectedRoles.[0].status", is("Failed")));
     }
 
     @Test
@@ -93,9 +88,11 @@ public class BulkServiceApplicationTests {
         int acceptedRoleCount = 1;
         roles.add(new Role(0, "SomeName", "Some desciption", "Some"));
         roles.add(new Role(0, "SomeName", "Some desciption", "Some"));
+        ZipEntry entry = new ZipEntry(toJsonString(roles));
         MockHttpServletRequestBuilder msb = post(urlTemplate)
-                .content(toJsonString(roles))
+                .content(entry.toString())
                 .contentType(MediaType.APPLICATION_JSON)
+                .header("Content-Encoding", "gzip")
                 .accept(MediaType.APPLICATION_JSON);
 
         // Act
@@ -189,6 +186,26 @@ public class BulkServiceApplicationTests {
         resultActions.andExpect(status().isOk());
     }
 
+    @Test
+    public void testCompression() throws Exception {
+
+        // Arrange
+        String json = "[{\"name\":\"name\",\"description\": \"descitption\",\"application\": \"application\"}]";
+        byte[] jsonb = compress(json.getBytes());
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.set("Content-Type", MediaType.APPLICATION_JSON.toString());
+        requestHeaders.set("Content-Encoding", "gzip");
+        HttpEntity<?> requestEntity = new HttpEntity<>(jsonb, requestHeaders);
+
+        // Act
+        ResponseEntity<BulkResponse> entity = this.restTemplate.exchange("http://localhost:" + port+CONTRACT_BASE_URI_WILCO, HttpMethod.POST,
+                requestEntity, BulkResponse.class);
+
+        // Assert
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(entity.getBody().getAcceptedRoles().size() == 1);
+    }
+
     /**
      * Convert {@link Object} type to JSON {@link String}.
      *
@@ -203,5 +220,29 @@ public class BulkServiceApplicationTests {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Compress source.
+     *
+     * @param source source to compress.
+     * @return compressed byte array.
+     */
+    private byte[] compress(byte[] source) throws IOException {
+        if (source == null || source.length == 0) {
+            return source;
+        }
+
+        ByteArrayInputStream sourceStream = new ByteArrayInputStream(source);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(source.length / 2);
+        OutputStream compressor = new GZIPOutputStream(outputStream);
+
+        try {
+            ByteStreams.copy(sourceStream, compressor);
+        } finally {
+            compressor.close();
+        }
+
+        return outputStream.toByteArray();
     }
 }
